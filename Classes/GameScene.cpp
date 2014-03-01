9e9e399d9d24a70cf53cb38466faead923596bcb
Kurose Nobuhito <enu.kuro@gmail.com>
2014-03-01 16:52:02 +0900

    add game scene

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -7,3 +7,18 @@
 //
 
 #include "GameScene.h"
+
+CCScene* GameScene::scene() {
+    CCScene *scene = CCScene::create();
+    GameScene *layer = GameScene::create();
+    scene->addChild(layer);
+    return scene;
+}
+
+bool GameScene::init() {
+    if (!CCLayer::init()) {
+        return false;
+    }
+    
+    return true;
+}
--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -10,5 +10,25 @@
 #define __GameJame__GameScene__
 
 #include <iostream>
+#include <iostream>
+#include "cocos2d.h"
+#include "cocos-ext.h"
+
+USING_NS_CC;
+USING_NS_CC_EXT;
+
+class GameScene : public cocos2d::CCLayer
+{
+private:
+    
+
+public:
+
+    virtual bool init();
+
+    static cocos2d::CCScene* scene();
+    
+    CREATE_FUNC(GameScene);
+};
 
 #endif /* defined(__GameJame__GameScene__) */
