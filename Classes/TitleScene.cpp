3f728c3a73eeb14877b057f826245a5690bd27b5
Kurose Nobuhito <enu.kuro@gmail.com>
2014-03-01 17:06:00 +0900

    add title scene

---
+++ Classes/TitleScene.cpp
@@ -0,0 +1,56 @@
+//
+//  TitleScene.cpp
+//  GameJame
+//
+//  Created by Kurose Nobuhito on 2014/03/01.
+//
+//
+
+#include "TitleScene.h"
+
+
+//
+//  TitleScene.cpp
+//
+//
+//  Created by Kurose Nobuhito on 2014/01/12.
+//
+//
+#include "TitleScene.h"
+#include "GameScene.h"
+
+
+CCScene* TitleScene::scene() {
+    CCScene *scene = CCScene::create();
+    TitleScene *layer = TitleScene::create();
+    scene->addChild(layer);
+    return scene;
+}
+
+bool TitleScene::init() {
+    if (!CCLayer::init()) {
+        return false;
+    }
+    
+    CCSize screenSize = CCDirector::sharedDirector()->getWinSize();
+    
+    CCSprite* splash = CCSprite::create("Default-568h@2x.png");
+    splash->setPosition(ccp(screenSize.width / 2, screenSize.height / 2));
+    this->addChild(splash, 1);
+    
+    CCFiniteTimeAction *seq1 = CCSequence::create(CCDelayTime::create(0.6),
+                                                  CCCallFunc::create(this,
+                                                                     callfunc_selector(TitleScene::finishTitleScene)), NULL);
+    
+    this->runAction(seq1);
+    
+    return true;
+}
+
+void TitleScene::finishTitleScene(){
+    
+    CCScene *scene = GameScene::scene();
+    
+    CCTransitionCrossFade *crossFade = CCTransitionCrossFade::create(0.2f, scene);
+    CCDirector::sharedDirector()->replaceScene(crossFade);
+}
---
+++ Classes/TitleScene.h
@@ -0,0 +1,30 @@
+//
+//  TitleScene.h
+//  GameJame
+//
+//  Created by Kurose Nobuhito on 2014/03/01.
+//
+//
+
+#ifndef __GameJame__TitleScene__
+#define __GameJame__TitleScene__
+
+
+#include <iostream>
+#include "cocos2d.h"
+
+class TitleScene : public cocos2d::CCLayer
+{
+public:
+    // Method 'init' in cocos2d-x returns bool, instead of 'id' in cocos2d-iphone (an object pointer)
+    virtual bool init();
+    
+    // there's no 'id' in cpp, so we recommend to return the class instance pointer
+    static cocos2d::CCScene* scene();
+    
+    void finishTitleScene();
+    
+    // preprocessor macro for "static create()" constructor ( node() deprecated )
+    CREATE_FUNC(TitleScene);
+};
+#endif /* defined(__GameJame__TitleScene__) */
